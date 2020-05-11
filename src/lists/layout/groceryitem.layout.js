e8f51117c0919f0b6965df1e9b51c78fb234e881
Bharat Gidwani <bharat.gidwani@gmail.com>
2020-05-10 22:51:53 -0400

    Adding add/delete functionality for grocery items

--- src/lists/layout/groceryitem.layout.js
+++ src/lists/layout/groceryitem.layout.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -13,6 +14,9 @@ import { red } from '@material-ui/core/colors';
 import IconButton from '@material-ui/core/IconButton';
 import GroceryListService from '../grocerylist.service';
 import { useGroceryListContext } from '../context/grocerylist-provider';
+import { CardActions, TextField } from '@material-ui/core';
+import Collapse from '@material-ui/core/Collapse';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const useStyles = makeStyles((theme) => ({
     card: { maxWidth: 300 },
@@ -24,14 +28,35 @@ const useStyles = makeStyles((theme) => ({
         height: 200,
         width: 200,
     },
-    chip: { margin: theme.spacing(0.5) },
+    chip: {
+        margin: theme.spacing(0.5),
+    },
+    expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+            duration: theme.transitions.duration.shortest,
+        }),
+    },
+    expandOpen: {
+        transform: 'rotate(180deg)',
+    },
+    textField: {
+        marginBottom: 20,
+    },
+    chip_done: {
+        margin: theme.spacing(0.5),
+        textDecorationLine: 'line-through',
+    },
 }));
 
 export default function GroceryItemCard({ listid, name, items }) {
     const classes = useStyles();
     const { refreshList, setToast } = useGroceryListContext();
+    const [expanded, setExpanded] = React.useState(false);
+    const [data, setData] = React.useState(items);
 
-    const handleDelete = () => {
+    const handleListDelete = () => {
         var result = window.confirm('Sure you want to delete the list?');
         if (result) {
             //delete the list
@@ -46,6 +71,73 @@ export default function GroceryItemCard({ listid, name, items }) {
         }
     };
 
+    const handleItemDelete = (item) => {
+        var result = window.confirm(
+            `Are you sure you want to remove ${item.name}?`
+        );
+        if (result) {
+            const newData = data.filter((srcitem) => srcitem._id !== item._id);
+            setData(newData);
+        }
+    };
+
+    const handleItemClick = (item) => {
+        // window.alert(`Clicked ${item.name}`);
+        const updatedData = data.map((el) =>
+            el._id === item._id ? Object.assign({}, el, { isdone: true }) : el
+        );
+        setData(updatedData);
+    };
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+    const handleSearchKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            const enteredText = e.target.value;
+            //add the item to the collection
+            // https://medium.com/javascript-in-plain-english/how-to-add-to-an-array-in-react-state-3d08ddb2e1dc
+            setData((data) => [...data, { name: enteredText }]);
+
+            //clear the entered text
+            e.target.value = '';
+            e.preventDefault();
+        }
+    };
+
+    const renderItems = (limititems) => {
+        const items = limititems ? data.slice(0, 5) : data;
+
+        return (
+            <Typography
+                align="center"
+                variant="body2"
+                color="textSecondary"
+                component="div"
+            >
+                {items.map((item) => {
+                    return (
+                        <Chip
+                            className={
+                                item.isdone ? classes.chip_done : classes.chip
+                            }
+                            label={item.name}
+                            size="medium"
+                            color="primary"
+                            onClick={
+                                expanded ? () => handleItemClick(item) : null
+                            }
+                            onDelete={
+                                expanded ? () => handleItemDelete(item) : null
+                            }
+                        />
+                    );
+                })}
+            </Typography>
+        );
+    };
+
     return (
         <Card className={classes.card}>
             <CardHeader
@@ -55,7 +147,7 @@ export default function GroceryItemCard({ listid, name, items }) {
                     </Avatar>
                 }
                 action={
-                    <IconButton aria-label="delete" onClick={handleDelete}>
+                    <IconButton aria-label="delete" onClick={handleListDelete}>
                         <DeleteIcon color="inherit" />
                     </IconButton>
                 }
@@ -75,24 +167,39 @@ export default function GroceryItemCard({ listid, name, items }) {
                     className={classes.media}
                     image="/assets/cart-checked.png"
                 />
-                <CardContent>
-                    <Typography
-                        align="center"
-                        variant="body2"
-                        color="textSecondary"
-                        component="div"
-                    >
-                        {items.slice(0, 5).map((item) => {
-                            return (
-                                <Chip
-                                    className={classes.chip}
-                                    label={item.name}
-                                    color="primary"
-                                />
-                            );
+                {!expanded && <CardContent> {renderItems(true)} </CardContent>}
+                <CardActions>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
                         })}
-                    </Typography>
-                </CardContent>
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography
+                            component="div"
+                            className={classes.textField}
+                        >
+                            <TextField
+                                name="search"
+                                placeholder="type to add item"
+                                id="search"
+                                variant="outlined"
+                                size="small"
+                                onKeyDown={handleSearchKeyPress}
+                            />
+                        </Typography>
+                        <Typography component="div">
+                            {renderItems(false)}
+                        </Typography>
+                    </CardContent>
+                </Collapse>
             </CardActionArea>
         </Card>
     );
